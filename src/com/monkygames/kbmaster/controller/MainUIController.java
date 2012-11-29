86da9a20f9c776f35ac021eb03484e3abce6e1f0
Michael Speth <conzar@users.sf.net>
2012-11-29 23:28:34 +1300

    Art assets are now complete!  Contining to build gui through scene builder.

---
+++ src/com/monkygames/kbmaster/controller/MainUIController.java
@@ -0,0 +1,43 @@
+/* 
+ * See COPYING in top-level directory.
+ */
+package com.monkygames.kbmaster.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+/**
+ * Handles UI Events for the main window.
+ * @version 1.0
+ */
+public class MainUIController implements Initializable{
+
+
+// ============= Class variables ============== //
+    @FXML
+    private Label versionL;
+// ============= Constructors ============== //
+// ============= Public Methods ============== //
+// ============= Protected Methods ============== //
+// ============= Private Methods ============== //
+// ============= Implemented Methods ============== //
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+// ============= Extended Methods ============== //
+// ============= Internal Classes ============== //
+// ============= Static Methods ============== //
+
+}
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
