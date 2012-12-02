d516f5a1ce66eb19dd9c56a92e78d8283bd03533
Michael Speth <conzar@users.sf.net>
2012-12-02 14:54:23 +1300

    Added a profile panel.

---
+++ src/com/monkygames/kbmaster/controller/ProfileUIController.java
@@ -0,0 +1,41 @@
+/* 
+ * See COPYING in top-level directory.
+ */
+package com.monkygames.kbmaster.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+/**
+ * Handles UI Events for the profile panel.
+ * @version 1.0
+ */
+public class ProfileUIController implements Initializable{
+
+
+// ============= Class variables ============== //
+// ============= Constructors ============== //
+// ============= Public Methods ============== //
+// ============= Protected Methods ============== //
+// ============= Private Methods ============== //
+// ============= Implemented Methods ============== //
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+// ============= Extended Methods ============== //
+// ============= Internal Classes ============== //
+// ============= Static Methods ============== //
+
+}
+/*
+ * Local variables:
+ *  c-indent-level: 4
+ *  c-basic-offset: 4
+ * End:
+ *
+ * vim: ts=8 sts=4 sw=4 noexpandtab
+ */
