6baaa9738d5c0015b39ab485f996f5e8f41d445c
Michael Speth <conzar@users.sf.net>
2013-03-19 22:46:07 +1300

    Fixed detecting key modifiers.

--- src/com/monkygames/kbmaster/controller/driver/SingleKeyController.java
+++ src/com/monkygames/kbmaster/controller/driver/SingleKeyController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,6 +21,7 @@ import javafx.stage.Stage;
 
 /**
  * Handles UI Events for the main window.
+ * TODO, use jinput to get key presses!
  * @version 1.0
  */
 public class SingleKeyController implements Initializable, EventHandler{
@@ -31,7 +33,9 @@ public class SingleKeyController implements Initializable, EventHandler{
     @FXML
     private TextField singleKeyTF;
     @FXML
-    private Label shiftL, ctrL, altL;
+    private Label shiftL, ctrlL, altL;
+    private boolean ignoreModifierRelease = false;
+
 // ============= Constructors ============== //
 // ============= Public Methods ============== //
     /**
@@ -42,9 +46,9 @@ public class SingleKeyController implements Initializable, EventHandler{
 	singleKeyTF.setText(singleKey);
     }
     public void setStage(Stage stage){
-	stage.addEventHandler(KeyEvent.KEY_TYPED, this);
+	//stage.addEventHandler(KeyEvent.KEY_TYPED, this);
 	//stage.addEventHandler(KeyEvent.KEY_PRESSED, this);
-	//stage.addEventHandler(KeyEvent.KEY_RELEASED, this);
+	stage.addEventHandler(KeyEvent.KEY_RELEASED, this);
 	//rootPane.addEventHandler(KeyEvent.KEY_TYPED, this);
     }
 // ============= Protected Methods ============== //
@@ -53,13 +57,44 @@ public class SingleKeyController implements Initializable, EventHandler{
     private void handleButtonAction(ActionEvent evt){
 	Object obj = evt.getSource();
     }
+    /**
+     * Sets the key pressed from the key event.
+     * @param keyEvent the key that was released.
+     * @param type the type of event, could by KEY, CTRL, SHIFT, ALT, or META.
+     */
+    private void setKeyPressed(KeyEvent keyEvent, String type){
+	System.out.println(type+" Pressed");
+	String key = keyEvent.getCharacter();
+	System.out.println("key: "+key+" Code: "+keyEvent.getCode()+" Text: "+keyEvent.getText());
+	KeyCode code = keyEvent.getCode();
+	singleKeyTF.setText(code.getName());
+	if(type.equals("ALT")){
+	    altL.setDisable(false);
+	    ctrlL.setDisable(true);
+	    shiftL.setDisable(true);
+	}else if(type.equals("CTRL")){
+	    altL.setDisable(true);
+	    ctrlL.setDisable(false);
+	    shiftL.setDisable(true);
+	}else if(type.equals("SHIFT")){
+	    altL.setDisable(true);
+	    ctrlL.setDisable(true);
+	    shiftL.setDisable(false);
+	}else{
+	    altL.setDisable(true);
+	    ctrlL.setDisable(true);
+	    shiftL.setDisable(true);
+	}
+    }
 // ============= Implemented Methods ============== //
     @Override
     public void initialize(URL url, ResourceBundle rb) {
     }
     @Override
     public void handle(Event event) {
-	System.out.println("Handle - "+event);
+	//System.out.println("Handle - "+event);
+
+
 	if(KeyEvent.KEY_TYPED.equals(event.getEventType())){
 	    KeyEvent keyEvent = (KeyEvent)event;
 	    System.out.println("KEY TYPED");
@@ -73,6 +108,7 @@ public class SingleKeyController implements Initializable, EventHandler{
 	    singleKeyTF.setText(key);
 	} else if(KeyEvent.KEY_PRESSED.equals(event.getEventType())){
 	    KeyEvent keyEvent = (KeyEvent)event;
+
 	    System.out.println("KEY Pressed");
 	    String key = keyEvent.getCharacter();
 	    System.out.println("key: "+key);
@@ -83,14 +119,32 @@ public class SingleKeyController implements Initializable, EventHandler{
 	    singleKeyTF.setText(key);
 	} else if(KeyEvent.KEY_RELEASED.equals(event.getEventType())){
 	    KeyEvent keyEvent = (KeyEvent)event;
-	    System.out.println("KEY Released");
-	    String key = keyEvent.getCharacter();
-	    System.out.println("key: "+key);
-	    System.out.println("is shift down: "+keyEvent.isShiftDown());
-	    System.out.println("is ctrl down: "+keyEvent.isControlDown());
-	    System.out.println("is alt down: "+keyEvent.isAltDown());
-	    System.out.println("is meta down: "+keyEvent.isMetaDown());
-	    singleKeyTF.setText(key);
+
+	    if (keyEvent.getCode() == KeyCode.CONTROL && !ignoreModifierRelease) {
+		setKeyPressed(keyEvent, "KEY");
+	    } else if (keyEvent.isControlDown()) {
+		ignoreModifierRelease = true;
+		setKeyPressed(keyEvent, "CTRL");
+	    }else if (keyEvent.getCode() == KeyCode.ALT && !ignoreModifierRelease) {
+		setKeyPressed(keyEvent, "KEY");
+	    } else if (keyEvent.isAltDown()) {
+		ignoreModifierRelease = true;
+		setKeyPressed(keyEvent, "ALT");
+	    }else if (keyEvent.getCode() == KeyCode.SHIFT && !ignoreModifierRelease) {
+		setKeyPressed(keyEvent, "KEY");
+	    } else if (keyEvent.isShiftDown()) {
+		ignoreModifierRelease = true;
+		setKeyPressed(keyEvent, "SHIFT");
+	    }else if (keyEvent.getCode() == KeyCode.META && !ignoreModifierRelease) {
+		setKeyPressed(keyEvent, "KEY");
+	    } else if (keyEvent.isMetaDown()) {
+		ignoreModifierRelease = true;
+		setKeyPressed(keyEvent, "META");
+	    } else if(!ignoreModifierRelease){
+		setKeyPressed(keyEvent, "KEY");
+	    } else {
+		ignoreModifierRelease = false;
+	    }
 	}
     }
 // ============= Extended Methods ============== //
