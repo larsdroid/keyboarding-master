dd1bcc96bd94629d38dec34f74859c779650ea12
Michael Speth <conzar@users.sf.net>
2013-08-08 21:22:00 +1200

    Updated scripts for developer preview & fixed a problem with Taipan UI.

--- scripts/dist.bash
+++ scripts/dist.bash
@@ -11,9 +11,12 @@ rm -rf $DEST_DIR
 #rm dist/kbmaster.tar.bz2
 
 mkdir dist/kbmaster
+mkdir dist/kbmaster/jre
 cp $DIST_DIR/KeyboardingMaster.jar dist/kbmaster/.
 cp -r $DIST_DIR/lib $DEST_DIR/.
 cp -r libs/native $DEST_DIR/lib/.
+cp -r libs/jre/jre1.7.0_21-linux-x64/java-linux-x64 $DEST_DIR/jre/.
+cp -r libs/jre/jre1.7.0_21-linux/java-linux $DEST_DIR/jre/.
 cp scripts/run.bash $DEST_DIR/.
 cp COPYING $DEST_DIR/.
 cp docs/readme.txt $DEST_DIR/.
