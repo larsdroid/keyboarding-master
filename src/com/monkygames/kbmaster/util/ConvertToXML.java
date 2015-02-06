185a61460a04ce1372d815c9dbc92675b3bfd085
Michael Speth <mspeth@monky-games.com>
2015-02-06 16:46:23 +1300

    KBM-28 had to add additional db4o configuration to make sure that everything was copied.

--- src/com/monkygames/kbmaster/util/ConvertToXML.java
+++ src/com/monkygames/kbmaster/util/ConvertToXML.java
@@ -2,10 +2,12 @@ package com.monkygames.kbmaster.util;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.EmbeddedObjectContainer;
+import com.db4o.config.EmbeddedConfiguration;
+import com.db4o.ta.TransparentActivationSupport;
+import com.monkygames.kbmaster.input.*;
+import com.monkygames.kbmaster.io.AppListManager;
 import com.monkygames.kbmaster.io.XStreamManager;
-import com.monkygames.kbmaster.profiles.AppType;
-import com.monkygames.kbmaster.profiles.Root;
-import com.monkygames.kbmaster.profiles.RootManager;
+import com.monkygames.kbmaster.profiles.*;
 import java.util.List;
 
 /**
@@ -15,7 +17,24 @@ public class ConvertToXML {
 
     public ConvertToXML(){}
     public boolean Convert(String db4oFile, String xmlFile){
-        EmbeddedObjectContainer db = Db4oEmbedded.openFile(db4oFile);
+        EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+        // turn on transparent activation
+        config.common().add(new TransparentActivationSupport());
+        // make sure that when a profile is updated so are all the data members
+        config.common().objectClass(Root.class).cascadeOnUpdate(true);
+        config.common().objectClass(App.class).cascadeOnUpdate(true);
+        config.common().objectClass(Profile.class).cascadeOnUpdate(true);
+        config.common().objectClass(Keymap.class).cascadeOnUpdate(true);
+        config.common().objectClass(Mapping.class).cascadeOnUpdate(true);
+        config.common().objectClass(ButtonMapping.class).cascadeOnUpdate(true);
+        config.common().objectClass(WheelMapping.class).cascadeOnUpdate(true);
+        config.common().objectClass(Button.class).cascadeOnUpdate(true);
+        config.common().objectClass(Wheel.class).cascadeOnUpdate(true);
+        config.common().objectClass(Output.class).cascadeOnUpdate(true);
+        config.common().objectClass(AppListManager.class).cascadeOnUpdate(true);
+        EmbeddedObjectContainer db = Db4oEmbedded.openFile(config, db4oFile);
+        //EmbeddedObjectContainer db = Db4oEmbedded.openFile(db4oFile);
+        // todo add cascade on read
         Root appsRoot = null;
         Root gamesRoot = null;
 	try{
@@ -34,6 +53,16 @@ public class ConvertToXML {
 	    }
 	}catch(Exception e){
             return false;
+        }
+
+        if(gamesRoot != null){
+            System.out.println("Games Root: "+gamesRoot.getName());
+            for(App app: gamesRoot.getList()){
+                app.printString();
+                for(Profile profile: app.getProfiles()){
+                    profile.printString();
+                }
+            }
         }
 
         RootManager rootManager = new RootManager(appsRoot, gamesRoot);
