9ebaf5b3b628fe682585a8bd03f099d8b29a6f80
Michael Speth <mspeth@monky-games.com>
2015-03-29 16:20:16 +1300

    KBM-39 updated version for next release.

--- src/com/monkygames/kbmaster/KeyboardingMaster.java
+++ src/com/monkygames/kbmaster/KeyboardingMaster.java
@@ -32,7 +32,7 @@ public class KeyboardingMaster extends Application {
 
     // === variables === //
     private LoginUIController controller;
-    public static final String VERSION = "0.4.0";
+    public static final String VERSION = "0.4.1";
 
     /**
      * Reference to this object.
